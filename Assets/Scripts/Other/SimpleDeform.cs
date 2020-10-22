a04c87def3129db07990f1871372fca1fd1e30f0
Valer-jan <72905449+Valer-jan@users.noreply.github.com>
2020-10-22 08:18:57 +0300

    simple deform voids Start and Update

--- Assets/Scripts/Other/SimpleDeform.cs
+++ Assets/Scripts/Other/SimpleDeform.cs
@@ -3,9 +3,15 @@
 public class SimpleDeform : MonoBehaviour
 {
     public Vector3 RotationSpeed;
+    Quaternion startRot;
+
+    private void Start()
+    {
+        startRot = transform.rotation;
+    }
 
     private void Update()
     {
-        
+        transform.rotation = startRot * Quaternion.Euler(RotationSpeed);
     }
 }
